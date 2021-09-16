711c9e0e9c162661506b3648af16fa2160d1e5a3
Bhanu Teja <18211a04h0@bvrit.ac.in>
2021-09-16 22:42:12 +0530

    Add prime section

---
+++ Educative/Competitive Programming in C++ The Keys to Success/prime/segmented_sieve.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ifstream cin("sieve.txt");
+
+    int n, m;
+    cin >> n >> m;
+    vector<int> is_prime(m-n+1, true);
+
+    for(int i = 2; i * i <= m; i++)
+    {
+        int start = (((n-1)/i) + 1) * i;
+        for(int j = start; j <= m; j+= i)
+        {
+            if (j >= n && j <= m)
+                is_prime[j-n] = false;
+        }
+    }
+    for(int i = 0; i < is_prime.size(); i++)
+        if (is_prime[i])
+            cout << i + n << " ";
+}
