fb52e85f57d14e9f5a826693996acb01c161e126
Bhanu Teja <18211a04h0@bvrit.ac.in>
2021-10-23 21:56:32 +0530

    Add week1 assignment

---
+++ Coursera/tensorflow/Course-1-Intro/task1.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+
+def house_model(y_new):
+    xs = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], dtype=float)
+    ys = np.array([1.0, 1.5, 2.0, 2.5, 3.0, 3.5], dtype=float)
+    model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+    model.compile(optimizer='sgd', loss='mean_squared_error')
+    model.fit(xs, ys, epochs=1400)
+    return model.predict(y_new)
+
+
+print(house_model([7.0]))
